597dfea6b4d644a80d70f7a1e76a3bf9ab190573
Jurga <jurgak@gmail.com>
2021-10-03 19:12:03 +0300

    4th commit - general summit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -23,25 +23,25 @@ public class Main {
         return pinCode == pinCodeMain;
     }
 
-    // method above: from what i understood, boolean generally can have either true or false value,
-    // and therefore boolean method returns true value if condition that is
-    // stipulated after 'return' is met;
-
     static void mainMenu() {
         Scanner input = new Scanner(System.in);
         int operation = selectOperation(input);
         switch (operation) {
-            case 1 -> displayBalance();
-            // above meaning is the same as
-            // case 1:
-            // displayBalance();
-            // break;
-            // improvement with '->' was suggested by IntelliJ;
-            // in fact IntelliJ didn't want to allow me to push to gitHub without this improvement
-            case 2 -> takeOutMoney();
-            case 3 -> deposit();
-            case 0 -> System.out.println("Bye!");
-            default -> System.out.println("Invalid choice");
+            case 1:
+                displayBalance();
+                break;
+            case 2:
+                takeOutMoney();
+                break;
+            case 3:
+                deposit();
+                break;
+            case 0:
+                System.out.println("Bye!");
+                break;
+            default:
+                System.out.println("Invalid choice");
+                break;
         }
     }
 
@@ -52,27 +52,33 @@ public class Main {
         System.out.println("Enter your choice: ");
         int screenPaper = input.nextInt();
         switch (screenPaper) {
-            case 1 -> System.out.println("Your current balance is: " + balanceMain + " EUR");
-            case 2 -> System.out.println("The planet is out of trees, no paper for you today");
-            default -> System.out.println("Invalid choice");
+            case 1:
+                System.out.println("Your current balance is: " + balanceMain + " EUR");
+                break;
+            case 2:
+                System.out.println("The planet is out of trees, no paper for you today");
+                break;
+            default:
+                System.out.println("Invalid choice");
+                break;
         }
     }
 
     static void takeOutMoney() {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the amount you would like to withdraw");
-        int sum = input.nextInt();
-        System.out.println((sum <= balanceMain) ? ("Please take your money: " + sum + " EUR") : ("Insufficient funds"));
-        if (sum <= balanceMain) {
-            System.out.println("Remaining balance: " + (balanceMain - sum) + " EUR");
+        int withdraw = input.nextInt();
+        System.out.println((withdraw <= balanceMain) ? ("Please take your money: " + withdraw + " EUR") : ("Insufficient funds. Your balance is " + balanceMain + " EUR"));
+        if (withdraw <= balanceMain) {
+            System.out.println("Remaining balance: " + (balanceMain - withdraw) + " EUR");
         }
     }
 
     static void deposit() {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the amount you would like to deposit: ");
-        int temp = input.nextInt();
-        System.out.println("Current balance: " + (balanceMain + temp) + " EUR");
+        int deposit = input.nextInt();
+        System.out.println(deposit + " EUR is transferred to your account");
     }
 
     static int selectOperation(Scanner input) {
